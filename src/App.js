fafa7e8550f5ef20994cc6b5f03e6b3874976609
samir-missaoui <samirmissaoui31@gmail.com>
2021-10-25 20:59:34 +0100

    react-checkpoint

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+import { ButtonToolbar } from "react-bootstrap";
+import { ButtonGroup } from "react-bootstrap";
+import { Form } from "react-bootstrap";
+import { Figure, Stack } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <Stack direction="vericale" gap={3}>
+        <Button variant="primary">Primary</Button>{" "}
+        <Button variant="secondary">Secondary</Button>{" "}
+        <Button variant="success">Success</Button>{" "}
+        <Button variant="warning">Warning</Button>{" "}
+        <Button variant="danger">Danger</Button>{" "}
+        <Button variant="info">Info</Button>{" "}
+        <Button variant="light">Light</Button>{" "}
+        <Button variant="dark">Dark</Button>{" "}
+        <Button variant="link">Link</Button>
+      </Stack>
+      <>
+        <Button variant="primary">Primary</Button>{" "}
+        <Button variant="secondary">Secondary</Button>{" "}
+        <Button variant="success">Success</Button>{" "}
+        <Button variant="warning">Warning</Button>{" "}
+        <Button variant="danger">Danger</Button>{" "}
+        <Button variant="info">Info</Button>{" "}
+        <Button variant="light">Light</Button>{" "}
+        <Button variant="dark">Dark</Button>{" "}
+        <Button variant="link">Link</Button>
+      </>
+      <ButtonToolbar aria-label="Toolbar with button groups">
+        <ButtonGroup className="me-2" aria-label="First group">
+          <Button>1</Button> <Button>2</Button> <Button>3</Button>{" "}
+          <Button>4</Button>
+        </ButtonGroup>
+        <ButtonGroup className="me-2" aria-label="Second group">
+          <Button>5</Button> <Button>6</Button> <Button>7</Button>
+        </ButtonGroup>
+        <ButtonGroup aria-label="Third group">
+          <Button>8</Button>
+        </ButtonGroup>
+      </ButtonToolbar>
+      <Form style={{ color: "green", backgroundColor: "black" }}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      <Figure>
+        <Figure.Image
+          width={171}
+          height={180}
+          alt="171x180"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/1200px-Bootstrap_logo.svg.png"
+        />
+        <Figure.Caption>
+          Nulla vitae elit libero, a pharetra augue mollis interdum.
+        </Figure.Caption>
+      </Figure>
+    </div>
+  );
+}
+
+export default App;
